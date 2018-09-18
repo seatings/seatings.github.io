e4fd0143263574bda5827b69590e0ceda3456b9f
crystrage1 <crystrage1@gmail.com>
2018-09-18 15:42:29 +0300

    commit

--- js/socket_seatmap_client.js
+++ js/socket_seatmap_client.js
@@ -4,7 +4,36 @@ const ioWebSeatmap = io.connect('http://api.evenz.co/client-api/web/seatmap');
 // Принимаю данные для запроса к Ticketevolution со стороны Сервера
 ioWebSeatmap.on("event-data-from-server", function(data) {
 
-    const ticketList = "Hello from seatmap.evenz.co";
+    // (hardcode) List of tickets received from api Ticket Evolution
+    const ticketList = [
+        {
+            "id": "1858306",
+            "price": "72.0",
+            "section": "PR15",
+            "row": "5",
+            "avail": "2",
+            "qty": "5",
+            "notes": "rien"
+        },
+        {
+            "id": "1858307",
+            "price": "62.0",
+            "section": "PR15",
+            "row": "6",
+            "avail": "2",
+            "qty": "7",
+            "notes": "rien"
+        },
+        {
+            "id": "1858307",
+            "price": "82.0",
+            "section": "PR15",
+            "row": "4",
+            "avail": "1",
+            "qty": "4",
+            "notes": "rien"
+        }
+    ];
 
     // Отправляю полученный список белетов из Web на Cервер 
     ioWebSeatmap.emit("tickets-list-from-web", ticketList);
